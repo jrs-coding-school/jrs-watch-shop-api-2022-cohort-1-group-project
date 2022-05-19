fd30df8c0001080d75a3d76d1b60c1cd0e312546
TommyZwick <tomzwick@hotmail.com>
2022-05-19 08:53:08 -0400

    Wrote SQL scripts for "watches" schema

---
+++ watchShop.sql
@@ -0,0 +1,61 @@
+DROP SCHEMA IF EXISTS `watches`;
+CREATE SCHEMA `watches`;
+
+CREATE TABLE `watches`.`product` (
+    `id` VARCHAR(50) NOT NULL UNIQUE,
+    `name` VARCHAR(255) NOT NULL,
+    `price` FLOAT NOT NULL,
+    `brand` VARCHAR(255) NOT NULL,
+    `color` VARCHAR(255) NOT NULL,
+    `style` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) DEFAULT NULL,
+    `image` BLOB NULL,
+    `rating` FLOAT DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `watches`.`user` (
+    `id` VARCHAR(50) NOT NULL UNIQUE,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `password` VARCHAR(255) NOT NULL,
+    `first_name` VARCHAR(255),
+    `last_name` VARCHAR(255),
+    `birthday` DATE,
+    `address1` VARCHAR(255),
+    `address2` VARCHAR(255),
+    `city` VARCHAR(255),
+    `state` VARCHAR(255),
+    `zip_code` INT,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `watches`.`transaction` (
+    `id` VARCHAR(50) NOT NULL UNIQUE,
+    `customer_id` VARCHAR(50) NOT NULL,
+    `date` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `total` FLOAT NOT NULL,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`customer_id`) 
+        REFERENCES `user`(`id`)
+);
+
+CREATE TABLE `watches`.`purchased_item` (
+    `id` VARCHAR(50) NOT NULL UNIQUE,
+    `product_id` VARCHAR(50) NOT NULL,
+    `quantity` INT NOT NULL,
+    `transaction_id` VARCHAR(255) NOT NULL,
+    `total` FLOAT NOT NULL,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`product_id`) 
+        REFERENCES `product`(`id`),
+    FOREIGN KEY (`transaction_id`) 
+        REFERENCES `transaction`(`id`)
+);
+
+INSERT INTO `watches`.`product` 
+    (id, `name`, price, brand, color, style)
+VALUES
+    ('1234-abcd-5678-ef90', 'G-SHOCK 123 abc', 1.99, 'G-SHOCK', 'red', 'wrist');
