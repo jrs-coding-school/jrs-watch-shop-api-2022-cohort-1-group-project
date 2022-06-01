6afefc946ce34f214aa31b5ffdd349c4981ef82a
Cjganderson1 <97189087+Cjganderson1@users.noreply.github.com>
2022-06-01 09:22:52 -0400

    added watches route

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./app/index');
+
 require('./app/routes/users.routes')(app);
 require('./app/routes/transactions.routes')(app);
 require('./app/routes/cartItems.routes')(app);
+require('./app/routes/watches.routes')(app);
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
