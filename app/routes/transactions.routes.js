2d4d7d61d4d44ea8f6d7e74ba931c5ed7f30bd22
nick johnson <njohnson144@hotmail.com>
2022-06-02 15:06:46 -0400

    fixed route for getting past orders

--- app/routes/transactions.routes.js
+++ app/routes/transactions.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
 
     const transactions = require('../controllers/transactions.controller');
 
-    app.get('/api/transactions/:userId', transactions.getAllTransactionsByUserId);
+    app.get('/api/transactions/user/:userId', transactions.getAllTransactionsByUserId);
     
     app.post('/api/transactions', transactions.createTransaction);
 
